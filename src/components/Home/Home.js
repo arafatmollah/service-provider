da4d8c01299f0d4f3322fda42f401dc68a2ea64c
MdArafatMollah303 <arafatshuvo18@gmail.com>
2022-06-14 21:12:58 +0600

    initial project with banner done

---
+++ src/components/Home/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Home.css'
+import { Button } from 'react-bootstrap';
+import Services from '../Services/Services';
+
+
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="banner mt-5 mb-5">
+                <div className="mt-5">
+                    <h1 className="fs-7"><span className="fs-10 fw-bold text-primary">Travel Agent</span> <br></br> Help you to find destination</h1>
+                    <p>If you are a travel peples who love yo travel all over the world then i am here for to to guide all over things about travel start with flight or by route service hotel/ motel destination place food guide you to where you should travel based on season.</p>
+                    <Button variant="success" className="p-2">Book Now</Button>
+                </div>
+                <div>
+                    <img className="rounded" src="https://media.istockphoto.com/photos/travel-planning-background-picture-id1309040743?b=1&k=20&m=1309040743&s=170667a&w=0&h=eyIzT1oSW2B5gPMPqgybEseIYIUrY96cxPTE_B0ewVs="></img>
+                </div>
+            </div>
+            <Services></Services>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h2 className="mt-5 text-center">Our Services</h2>
+        </div>
+    );
+};
+
+export default Services;
