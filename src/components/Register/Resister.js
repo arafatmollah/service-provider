98e054327a74a756bfa27d1e054e8586f68a769a
MdArafatMollah303 <arafatshuvo18@gmail.com>
2022-06-15 17:34:53 +0600

    firebase hook added

--- src/components/Register/Resister.js
+++ src/components/Register/Resister.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import {getAuth} from 'firebase/auth';
+import app from '../../firebase.init';
 
+const auth = getAuth(app);
 const Resister = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth);
+
+  if (error) {
     return (
-        <div>
-            <h2>Registration Your Details</h2>
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (user) {
+    return (
+      <div>
+        <p>Registered User: {user.email}</p>
+      </div>
+    );
+  }
+    return (
+        <div className="container shadow mt-4 p-3 mb-5 bg-body rounded">
+            <h2 className='text-center text-primary mt-2'>Registration Your Details</h2>
+
+            <div className="">
+            <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+  <Form.Label>Enter your full Name</Form.Label>
+  <Form.Control type="name" placeholder="Enter name" />
+    <Form.Label>Email address</Form.Label>
+
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button onClick={() => createUserWithEmailAndPassword(email, password)} variant="primary" type="submit">
+    Submit
+  </Button>
+  <Button>SignUP with Google</Button>
+</Form>
+            </div>
         </div>
     );
 };
