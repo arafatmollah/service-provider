067a9677eacfd6f59dca132a0cb065a6b85426e9
MdArafatMollah303 <arafatshuvo18@gmail.com>
2022-06-16 15:24:27 +0600

    display user added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import './Header.css';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {getAuth, signOut} from 'firebase/auth';
+import app from '../../firebase.init';
+
+const auth = getAuth(app);
 
 const Header = () => {
+    const [user] = useAuthState(auth);
     return (
-    
+        
         <nav className="container">
          <Link to='/'>Home</Link>   
         <Link to='/blogs'>Blogs</Link>  
         <Link to="/checkout">Checkout</Link>  
-        <Link className="login" to='login'>Log In</Link>   
+        <span>{user?.displayName && user?.displayName}</span>
+        {
+            user?.uid
+            ?
+            <Button onClick={()=>signOut(auth)}>Sign Out</Button>
+            :
+            <Link className="login" to='login'>Log In</Link>
+        }   
         <Link className="register" to='/register'>Register</Link> 
         
         </nav> 
